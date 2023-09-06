2652226bdff0fa68a4d88cc58da7681d072f5d29
Joey Remmers <joeyjoeyremmers@gmail.com>
2023-09-06 16:08:36 +0200

    added collision script

--- Assets/Scrips/PlayerControls.cs
+++ Assets/Scrips/PlayerControls.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mover : MonoBehaviour
+public class PlayerControls : MonoBehaviour
 {
     private float inputHorizontal = 0;
     private float inputVertical = 0;
     private float inputJump = 0;
 
+    [Header("Speed modifiers")]
     [SerializeField] float HorizontalSpeedModifier = 1;
     [SerializeField] float VerticalSpeedModifier = 1;
     [SerializeField] float JumpSpeedModifier = 1;
+
+    Vector2 turn;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +23,22 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        RotatePlayer();
+    }
+
+    void MovePlayer()
+    {
         inputHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * HorizontalSpeedModifier;
         inputVertical = Input.GetAxis("Vertical") * Time.deltaTime * VerticalSpeedModifier;
         inputJump = Input.GetAxis("Jump") * Time.deltaTime * JumpSpeedModifier;
         
         transform.Translate(inputHorizontal,inputJump,inputVertical);
+    }
+
+    void RotatePlayer()
+    {
+        turn.x += Input.GetAxis("Mouse X");
+        transform.localRotation = Quaternion.Euler(0, turn.x, 0);
     }
 }
